90d1eeb5019c85cd8f13865b80e794e177f7f67d
swamfire <usern5949@gmail.com>
2023-09-07 01:26:24 +0530

    Added overlay display and hide feature

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 'use strict';
 
+const closeModal = function(){
+    modal.classList.add("hidden")
+    overlay.classList.add("hidden")
+}
 
+const opemModal = function(){
+    modal.classList.remove("hidden")
+    overlay.classList.remove("hidden")
+}
 const modal = document.querySelector(".modal")
 const overlay = document.querySelector(".overlay")
 const btnCloseModal = document.querySelector(".close-modal")
@@ -9,4 +17,23 @@ console.log(btnsOpenModal)
 
 for (let i=0;i<btnsOpenModal.length;i++){
     console.log(btnsOpenModal[i].textContent)
+    btnsOpenModal[i].addEventListener("click", opemModal)
+    // btnsOpenModal[i].addEventListener("click", ()=>{
+    //     // console.log(`${btnsOpenModal[i].textContent} Clicked`)
+    //     modal.classList.remove("hidden")
+    //     overlay.classList.remove("hidden")
+    //     // modal.computedStyleMap.display = "block"
+    // })
 }
+btnCloseModal.addEventListener("click",closeModal)
+overlay.addEventListener("click",closeModal)
+// btnCloseModal.addEventListener("click", ()=>{
+//     console.log(`${btnCloseModal.textContent} Clicked`)
+//     modal.classList.add("hidden")
+//     overlay.classList.add("hidden")
+// })
+// overlay.addEventListener("click",() =>{
+//     console.log(`${overlay.textContent} Clicked`)
+//     modal.classList.add("hidden")
+//     overlay.classList.add("hidden")
+// })
